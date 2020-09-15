d15f87b75f097c04948aa88028d5bc866259ed0b
duanjianhui <duanjianhui@xiaomi.com>
2020-09-15 14:20:47 +0800

    coding in 2020-09-15
    T-BD-1
    
    Signed-off-by: duanjianhui <duanjianhui@xiaomi.com>

---
+++ src/com/company/common/P922.java
@@ -0,0 +1,24 @@
+package com.company.common;
+
+public class P922 {
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public int[] sortArrayByParityII(int[] A) {
+        int j = 1;
+        for (int i = 0; i < A.length; i += 2) {
+            if (A[i] % 2 == 1) {
+                while (A[j] % 2 == 1) {
+                    j += 2;
+                }
+                swap(A, i, j);
+            }
+        }
+        return A;
+    }
+
+}
